bf7b29cd9b41fc86418a793ee4445540923955d1
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-28 03:29:59 +0400

    Refactor: Lasnguage checker functionality added

--- components/shared/LanguageChanger.jsx
+++ components/shared/LanguageChanger.jsx
@@ -1,30 +1,39 @@
+import { Link } from "@/i18n/routing";
 import { useLocale } from "next-intl";
 import React, { useState } from "react";
 import { GrLanguage } from "react-icons/gr";
+
 const LanguageChanger = () => {
   const [show, setShow] = useState(false);
   const locale = useLocale();
+
   return (
     <div className="flex items-center gap-2 text-sm">
       {show && (
         <div>
-          <button
+          <Link
+            href="/"
+            locale="en"
+            replace
             type="button"
             className={`${locale === "en" ? "text-emerald-500" : ""}`}
           >
             EN
-          </button>
+          </Link>
           <span className="mx-2 opacity-50">/</span>
-          <button
+          <Link
+            href="/"
+            locale="az"
+            replace
             type="button"
             className={`${locale === "az" ? "text-green-500" : ""}`}
           >
             AZ
-          </button>
+          </Link>
         </div>
       )}
       <button type="button" onClick={() => setShow(!show)}>
-        <GrLanguage className=" w-6 sm:w-8 h-6 sm:h-8" />
+        <GrLanguage className="w-6 sm:w-8 h-6 sm:h-8" />
       </button>
     </div>
   );
