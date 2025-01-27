0d463d915e90ed855bda1144a4a02b5c7a6fddc4
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-28 03:53:13 +0400

    Feature: Filter functionality added

--- components/shared/RepoList.jsx
+++ components/shared/RepoList.jsx
@@ -1,20 +1,27 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Input from "../common/Input";
 import Repo from "./Repo";
 import { REPO_LIST } from "@/utils/constants";
 
 const RepoList = () => {
-  const data = REPO_LIST;
+  const [filterBy, setFilterBy] = useState("");
+  const data = filterBy
+    ? REPO_LIST.filter((e) => e.tech.includes(filterBy.toLocaleLowerCase()))
+    : REPO_LIST;
   return (
     <div className="w-full h-full py-4">
       <div className="px-6">
-        <Input />
+        <Input
+          type="search"
+          name="search"
+          onChange={(e) => setFilterBy(e.target.value)}
+        />
       </div>
       <div className="h-full overflow-y-auto">
         <div className="px-6">
           {data?.map((repo, index) => (
-            <Repo url={repo} key={index} />
+            <Repo url={repo.url} key={index} />
           ))}
         </div>
       </div>
--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,26 @@
 export const REPO_LIST = [
-  "https://api.github.com/repos/Anrsgrl/next-intl-js",
-  "https://api.github.com/repos/Anrsgrl/next-intl-ts",
-  "https://api.github.com/repos/Anrsgrl/next-intl-zustand-js",
-  "https://api.github.com/repos/Anrsgrl/next-intl-zustand-ts",
-  "https://api.github.com/repos/Anrsgrl/next-firebase-js",
-  "https://api.github.com/repos/Anrsgrl/next-firebase-ts",
+  {
+    url: "https://api.github.com/repos/Anrsgrl/next-intl-js",
+    tech: "nextjs, tailwindcss, next-intl, javascript",
+  },
+  {
+    url: "https://api.github.com/repos/Anrsgrl/next-intl-ts",
+    tech: "nextjs, tailwindcss, next-intl, typescript",
+  },
+  {
+    url: "https://api.github.com/repos/Anrsgrl/next-intl-zustand-js",
+    tech: "nextjs, tailwindcss, next-intl, zustand, javascript",
+  },
+  {
+    url: "https://api.github.com/repos/Anrsgrl/next-intl-zustand-ts",
+    tech: "nextjs, tailwindcss, next-intl, zustand, typescript",
+  },
+  {
+    url: "https://api.github.com/repos/Anrsgrl/next-firebase-js",
+    tech: "nextjs, tailwindcss, firebase, javascript",
+  },
+  {
+    url: "https://api.github.com/repos/Anrsgrl/next-firebase-ts",
+    tech: "nextjs, tailwindcss, firebase, typescript",
+  },
 ];
