f6f014533d9684a2c4ca2f57eac8fa723b67df30
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-28 02:22:11 +0400

    Style: MacOS button sizes decreased

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -10,13 +10,13 @@ export default function Home() {
     <div className="p-4 flex justify-center items-center container mx-auto h-[95lvh] overflow-hidden">
       <div className="w-full h-[90lvh] bg-gradient-to-br bg-gradient-to-br from-zinc-950 via-zinc-950 to-zinc-900 rounded-md relative border-2 border-zinc-300">
         <div className="relative flex items-center gap-2 w-full border-b-2 border-zinc-300 py-4 px-4 bg-zinc-800">
-          <div className="rounded-full w-6 h-6 bg-mac-red"></div>
-          <div className="rounded-full w-6 h-6 bg-mac-yellow"></div>
-          <div className="rounded-full w-6 h-6 bg-mac-green"></div>
+          <div className="rounded-full w-4 h-4 bg-mac-red"></div>
+          <div className="rounded-full w-4 h-4 bg-mac-yellow"></div>
+          <div className="rounded-full w-4 h-4 bg-mac-green"></div>
         </div>
         <div className="py-4 pb-0 h-3/4">
           <GithubTop active={activeTab} setActive={setActiveTab} />
-          <div className="h-full  px-6">
+          <div className="h-full">
             {activeTab === "Templates" ? <RepoList /> : <Overview />}
           </div>
         </div>
