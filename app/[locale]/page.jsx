6023afdfb755c79d13d146676f17838f2fd49236
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-28 04:08:40 +0400

    Style: macos button background changed

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="p-4 flex justify-center items-center container mx-auto h-[95lvh] overflow-hidden">
       <div className="w-full h-[90lvh] bg-gradient-to-br bg-gradient-to-br from-zinc-950 via-zinc-950 to-zinc-900 rounded-md relative border-2 border-zinc-300">
-        <div className="relative flex items-center gap-2 w-full border-b-2 border-zinc-300 py-4 px-4 bg-zinc-800">
+        <div className="relative flex items-center gap-2 w-full border-b-2 border-zinc-300 py-4 px-4 bg-gradient-to-r from-zinc-900 to-zinc-950 ">
           <div className="rounded-full w-4 h-4 bg-mac-red"></div>
           <div className="rounded-full w-4 h-4 bg-mac-yellow"></div>
           <div className="rounded-full w-4 h-4 bg-mac-green"></div>
